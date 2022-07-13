00849e8598b15e2b7482f1757ef77cafec198c07
sabithakuppusamy <sabi95oct@gmail.com>
2022-07-13 10:16:59 +0530

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex flex-col  justify-center min-h-[100vh] align-middle">
+      <h1 className="text-3xl font-thin text-white">Hello world!</h1>
+    </div>
+  );
+};
+
+export default Home;
